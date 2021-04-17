093de3263e222d8d799332171a7e3f0cb9a56e73
a7me3D <ahmedjbeli54@yahoo.fr>
2021-04-18 00:08:58 +0100

    delete + add  category

--- routes/category.js
+++ routes/category.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router()
 
-const {getCategories} = require("../controllers/category")
+const {getCategories, deleteCategory, addCategory} = require("../controllers/category")
 
 router.get("/", getCategories)
+router.post("/delete/:id", deleteCategory)
+router.post("/add", addCategory)
 
 
 module.exports = router
