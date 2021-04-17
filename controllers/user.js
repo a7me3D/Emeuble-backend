9860aabbb14c249771ea0b86202cb832984cfabe
a7me3D <ahmedjbeli54@yahoo.fr>
2021-04-18 00:41:17 +0100

    getUsers + updateUsers

--- controllers/user.js
+++ controllers/user.js
@@ -27,3 +27,20 @@ exports.getUser = (req, res) => {
       });
     });
   }
+
+exports.getUsers = async (req,res) =>{
+  try{
+    const users = await User.find().lean()
+    return res.status(200).json(users)
+  }catch{
+      return res.status(500).json({message:"Internal server error"})
+  }
+}
+
+exports.updateUser = (req, res) => {
+  const userId = req.params.id
+  User.findByIdAndUpdate(userId, (err, user) => {
+    if(err) return res.status(500).json({message:"Internal server error"})
+    else return res.status(200).json({message:"User updated"})
+  })
+}
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
-const { getUser } = require("../controllers/user")
+const { getUser, getUsers, updateUser } = require("../controllers/user")
 const express = require("express");
 const router = express.Router()
 const auth = require("../middleware/jwt")
 
 router.get("/:uid", auth.checkToken , getUser);
+router.get("/", auth.checkToken , getUsers);
+router.post("/", auth.checkToken , updateUser);
 
 
 module.exports = router
