8e1bae646f02e19d9180b2d91a60b0730f3a5d1e
a7me3D <ahmedjbeli54@yahoo.fr>
2021-04-19 17:11:45 +0100

    command schema

---
+++ models/command.js
@@ -0,0 +1,14 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var commandSchema = new Schema({
+  userId: {type: Schema.Types.ObjectId, ref: 'User'},
+  productsId: [{ type : Schema.Types.ObjectId, ref: 'Product' }],
+  status: { type: Boolean, required: true, default:false },
+  commandPrice: { type: Number, required: true },
+  commandtDate: Date,
+  
+});
+
+
+module.exports = mongoose.model("Command", commandSchema);
