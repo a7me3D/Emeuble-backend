eede2a8f38e92f67f6f972ff063fad6471450457
a7me3D <ahmedjbeli54@yahoo.fr>
2021-04-19 14:44:12 +0100

    create + read messages

---
+++ controllers/message.js
@@ -0,0 +1,24 @@
+const Message = require("../models/message")
+
+exports.addMessage = (req, res) => {
+    const message = new Message()
+    message.messageName = req.body.name
+    message.messageEmail = req.body.email
+    message.messageSubject = req.body.subject
+    message.messageContent = req.body.content
+    message.messageDate = Date.now()
+
+    message.save((err, msg)=>{
+        if(err)return res.status(500).json({message:"Internal server error"})
+        else return res.status(200).json({message:"Message sent"})
+    })
+}
+
+exports.getMessages = async  (req, res) => {
+    try{
+        const messages = await Message.find().lean()
+        return res.status(200).json(messages)
+    }catch{
+        return res.status(500).json({message:"Internal server error"})
+    }
+}
