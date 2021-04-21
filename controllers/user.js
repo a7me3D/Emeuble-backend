ec58db248af49ebcc4ff0a68b5fbfaeea181f266
a7me3D <ahmedjbeli54@yahoo.fr>
2021-04-21 15:39:33 +0100

    fix update user

--- controllers/user.js
+++ controllers/user.js
@@ -39,7 +39,7 @@ exports.getUsers = async (req,res) =>{
 
 exports.updateUser = (req, res) => {
   const userId = req.params.id
-  User.findByIdAndUpdate(userId, (err, user) => {
+  User.findByIdAndUpdate(userId, req.body,  (err, user) => {
     if(err) return res.status(500).json({message:"Internal server error"})
     else return res.status(200).json({message:"User updated"})
   })
