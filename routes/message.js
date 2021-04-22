f9bee9798e4111d94e354fbcf828285815cbf846
a7me3D <ahmedjbeli54@yahoo.fr>
2021-04-22 22:05:13 +0100

    fix func call

--- routes/message.js
+++ routes/message.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router()
 const { isAdmin } = require("../middleware/admin");
-const { getMessages, addMessage, getMessagesByEmail } = require("../controllers/message");
+const { getMessages, addMessage, getMessagesByEmail, deleteMessage } = require("../controllers/message");
 const auth = require("../middleware/jwt")
 
 router.get("/", auth.checkToken , isAdmin, getMessages)
 router.post("/send", addMessage)
 router.get("/me", auth.checkToken, getMessagesByEmail)
-router.delete("/:id", auth.checkToken, addMessage)
+router.delete("/:id", auth.checkToken, deleteMessage)
 
 
 module.exports = router
