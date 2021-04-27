742383b83ca4b7d73f6237e0c64098fdb6e2da97
a7me3D <ahmedjbeli54@yahoo.fr>
2021-04-27 23:32:33 +0100

    add command fields phone + adress

--- models/command.js
+++ models/command.js
@@ -5,6 +5,8 @@ var commandSchema = new Schema({
   userId: {type: Schema.Types.ObjectId, ref: 'User'},
   productsId: [{ type : Schema.Types.ObjectId, ref: 'Product' }],
   status: { type: Boolean, required: true, default:false },
+  commandAdress: { type: String, required: true},
+  commandPhone: { type: String, required: true},
   commandPrice: { type: Number, required: true },
   commandDate: Date,
   
